59af8254731a19ab0eed2780fd6691d73ffa478a
Icia Colomar <iciacolomar@apeteat.com>
2020-08-13 12:22:10 +0200

    create functionallity

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 'use strict';
 
-const quoteContainer =  document.getElementById('quote-container');
+const quoteContainer =  document.querySelector('.quote-container');
 const quoteText =  document.getElementById('quote');
 const authorText =  document.getElementById('author');
-const twitterBtn =  document.getElementById('twitter');
-const newQuoteBtn =  document.getElementById('new-quote');
+const twitterBtn =  document.querySelector('.btn-twitter');
+const newQuoteBtn =  document.querySelector('.new-quote');
+const loader = document.querySelector('.loader');
+
+// Loading Spinner Show
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Remove Loading
+function completeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Code from API
 async function getQuote() {
+    showLoadingSpinner();
     const proxyURL = 'https://cors-anywhere.herokuapp.com/';
     const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
     try {
         const response = await fetch(proxyURL + apiURL);
         const data = await response.json();
-
+        console.log(data)
         data.quoteAuthor === '' ? authorText.innerText = 'Unknow' : authorText.innerText = data.quoteAuthor;
         quoteText.innerText = data.quoteText;
+
+        // Stop loader and show Quote
+        completeLoadingSpinner();
+        
     } catch (error) {
         getQuote();
         console.log('LOL, no quote', error)
